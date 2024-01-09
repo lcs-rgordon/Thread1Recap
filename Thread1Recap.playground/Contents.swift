357b975f5a5485b28c8e940a79f1ee6138cdb727
Russell Gordon <rgordon@lcs.on.ca>
2024-01-09 11:41:29 -0500

    Answers to the first couple of questions from the recap.

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,28 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Vocabulary
+//
+// - variable
+// - constant
+// - data type
+//   ...examples include:
+//      String
+//      Int
+//      Float
+//      Double
+//      Bool
+// - structure
+// - property
+// - instance
+
+
+var age = 16
+let acclerationDueToGravity = 9.8
+
+// 1. "age" is an Int
+//
+//    We can Option-click to find the inferred data type if we are not sure.
+
+// 2. "acclerationDueToGravity" is a Double.
+
+
